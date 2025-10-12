1c77d50a02db13ca4af04d9fdae0e36c2af49649
DESKTOP-6N0TNJM\ADMIN <vientlnse184359@fpt.edu.vn>
2025-10-12 21:11:32 +0700

    initial commit

---
+++ src/main/java/com/swd392/BatterySwapStation/BatterySwapStationApplication.java
@@ -0,0 +1,13 @@
+package com.swd392.BatterySwapStation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatterySwapStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatterySwapStationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swd392/BatterySwapStation/BatterySwapStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swd392.BatterySwapStation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatterySwapStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
