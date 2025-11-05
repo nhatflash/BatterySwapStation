5c1ee3a23135d9986766415653dc10f352671e57
nhatflash <tranlenhat123456@gmail.com>
2025-11-05 09:32:37 +0700

    no first swap price

--- src/main/java/com/swd392/BatterySwapStation/application/common/shared/PriceCalculator.java
+++ src/main/java/com/swd392/BatterySwapStation/application/common/shared/PriceCalculator.java
@@ -4,5 +4,5 @@ public class PriceCalculator {
 
     public static final double SWAP_PRICE = 100000.0;
 
-    public static final double FIRST_SWAP_PRICE = 1000000.0;
+    public static final double FIRST_SWAP_PRICE = 100000.0;
 }
